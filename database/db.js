40e65129df7a8cdb2ced1f6926d6aa01af03fa59
ZefianAlfian <70304409+ZefianAlfian@users.noreply.github.com>
2021-02-15 09:11:32 +0700

    rm: database url

--- database/db.js
+++ database/db.js
@@ -3,7 +3,7 @@ var __path = process.cwd(),
      { color } = require(__path + '/lib/color.js')
 
 // Connection URL
-var url = 'mongodb+srv://zefianap:Zefian7387@zapi.voj3i.mongodb.net/zefian_rapi?retryWrites=true&w=majority';
+var url = '***';
 
 var db = monk(url);
 
